d39dede72c2bb812829d6310f52b807eafc4e16f
tjanssen-ssg <152877816+tjanssen-ssg@users.noreply.github.com>
2023-12-04 16:13:25 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -7,7 +7,10 @@ public class Greeter {
   public Greeter() {
 
   }
-
+/**
+* @param name someone of a person
+* @return freeting string
+**/
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
